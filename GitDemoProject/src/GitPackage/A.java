b52d3497f7b3b0488c396f418707a1e1702a0cb3
suraj26-git <71366979+suraj26-git@users.noreply.github.com>
2020-09-16 16:29:51 +0530

    Update A.java
    by git account

--- GitDemoProject/src/GitPackage/A.java
+++ GitDemoProject/src/GitPackage/A.java
@@ -5,6 +5,9 @@ public class A {
 	public void sum() {
 		System.out.println("sum==A");
 	}
+	public void sub() {
+		System.out.println("sub==A");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello...");
 	}
