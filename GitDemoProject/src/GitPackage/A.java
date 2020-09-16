02e265f77cfb618dffbf575374f2539f2ca1076e
Shree <Shree@DESKTOP-C63B4ID>
2020-09-16 16:09:16 +0530

    change by suraj

--- GitDemoProject/src/GitPackage/A.java
+++ GitDemoProject/src/GitPackage/A.java
@@ -1,6 +1,10 @@
 package GitPackage;
 
 public class A {
+	
+	public void sum() {
+		System.out.println("sum==A");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello...");
 	}
